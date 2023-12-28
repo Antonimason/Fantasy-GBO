8c1511329c7f6db984b8a83b416771315b80417b
Antonimason <sba23108@student.cct.ie>
2023-12-28 18:40:46 +0000

    first commit

---
+++ src/main/java/com/apache/poi/fantasygbo/FantasyGBO.java
@@ -0,0 +1,129 @@
+package com.apache.poi.fantasygbo;
+
+
+import java.util.Scanner;
+
+public class FantasyGBO {
+    
+    //MAIN MENU
+    public static String Start(){
+        System.out.println("Press \n 1 - Add Player and Contestants \n 2 - Add weekly points \n 3 - Check prediction \n 4 - check result \n 5 - Check the winner \n 6 - Exit");
+        Scanner playerSelection = new Scanner(System.in);
+        String starting = playerSelection.nextLine();
+        return starting;
+    }
+    
+    //POINTS MENU
+    public static String weekElection() {
+        System.out.println("Press \n 1 - to add points in the week  \n 2 - to add points in the week 2 \n 3 - to add points in the week 3 \n 4 - to add points in the result list \n  5 - Exit");
+        Scanner weekSelection = new Scanner(System.in);
+        String week = weekSelection.nextLine();
+        return week;
+    }
+    public static void main(String[] args) {
+       
+        boolean trigger = false;
+        String name1;
+        String contestant1;
+        String contestant2;
+        String contestant3;
+        String contestant4;
+        String contestant5;
+        String contestant6;
+        String contestant7;
+        String contestant8;
+        String contestant9;
+                
+        while(trigger == false){
+        String menu = Start();
+        
+        switch(menu){
+        case "1":
+            System.out.println("You choose to add a player");
+            try{ 
+                Scanner playerName = new Scanner(System.in);
+                System.out.println("Give me a name of the first player");
+                name1 = playerName.nextLine();
+                System.out.println("Give me a name of 3 contestant");
+                contestant1 = playerName.nextLine();
+                contestant2 = playerName.nextLine();
+                contestant3 = playerName.nextLine();
+                System.out.println("Give me a name of the second player");
+                String name2 = playerName.nextLine();
+                System.out.println("Give me a name of 3 the contestant");
+                contestant4 = playerName.nextLine();
+                contestant5 = playerName.nextLine();
+                contestant6 = playerName.nextLine();
+                System.out.println("Give me a name of the third player");
+                String name3 = playerName.nextLine();
+                System.out.println("Give me a name of 3 the contestant");
+                contestant7 = playerName.nextLine();
+                contestant8 = playerName.nextLine();
+                contestant9 = playerName.nextLine();
+
+                System.out.println("Players and Contestants have been added to the list");
+                Workbook.workbook(name1, name2, name3, contestant1, contestant2, contestant3, contestant4, contestant5, contestant6, contestant7, contestant8, contestant9);
+                break;
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        case "2":
+            try{
+                String week = weekElection();
+                switch(week){
+                    case "1":
+                        Workbook.modifyBook("fantasy.csv","week1");
+                        break;
+                    case "2":
+                        Workbook.modifyBook("fantasy.csv","week2");
+                        break;
+                    case "3":
+                        Workbook.modifyBook("fantasy.csv","week3");
+                        break;
+                    case "4":
+                        Workbook.modifyBook("fantasy.csv","result");
+                        break;
+                    case "5":
+                        Start();
+                        break;
+                    default:
+                        System.out.println("You must press a number between 1 - 5");
+                        weekElection();
+                }
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        case "3":
+            try{
+               Workbook.getBook("fantasy.csv");
+            }catch(Exception e){
+                System.out.println(e);
+            };
+            break;
+        case "4":
+            System.out.println("Here is the result list");
+            try{
+               Workbook.getResult("fantasy.csv");
+            break;
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        case "5":
+            try{
+                Workbook.getWinner("fantasy.csv");
+            
+            break;
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        case "6":
+            trigger = true;
+            break;
+        default:
+            System.out.println("You must press a number between 1 - 6");
+            Start();
+        }
+        
+        } 
+    }
+}
---
+++ src/main/java/com/apache/poi/fantasygbo/Workbook.java
@@ -0,0 +1,259 @@
+package com.apache.poi.fantasygbo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.RichTextString;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+public class Workbook {
+
+    public static void workbook(String player1, String player2, String player3, String contestant1, String contestant2, String contestant3, String contestant4, String contestant5, String contestant6, String contestant7, String contestant8, String contestant9) {
+        try {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet sheet = workbook.createSheet("FantasyGBO");
+
+            HSSFRow rowhead = sheet.createRow((short) 0);
+            rowhead.createCell(0).setCellValue("Player");
+            rowhead.createCell(1).setCellValue("Contestant");
+            rowhead.createCell(2).setCellValue("Week 1");
+            rowhead.createCell(3).setCellValue("Week 2");
+            rowhead.createCell(4).setCellValue("Week 3");
+            rowhead.createCell(5).setCellValue("Total");
+            rowhead.createCell(6).setCellValue("Result");
+
+            HSSFRow row1 = sheet.createRow((short) 1);
+            row1.createCell(0).setCellValue(player1);
+            row1.createCell(1).setCellValue(contestant1);
+            HSSFCell cell1 = row1.createCell(5);
+            cell1.setCellFormula("SUM(C2:E2)");
+            
+            HSSFRow row2 = sheet.createRow((short) 2);
+            row2.createCell(0).setCellValue(player1);
+            row2.createCell(1).setCellValue(contestant2);
+            HSSFCell cell2 = row2.createCell(5);
+            cell2.setCellFormula("SUM(C3:E3)");
+
+            HSSFRow row3 = sheet.createRow((short) 3);
+            row3.createCell(0).setCellValue(player1);
+            row3.createCell(1).setCellValue(contestant3);
+            HSSFCell cell3 = row3.createCell(5);
+            cell3.setCellFormula("SUM(C4:E4)");
+
+            HSSFRow row4 = sheet.createRow((short) 4);
+            row4.createCell(0).setCellValue(player2);
+            row4.createCell(1).setCellValue(contestant4);
+            HSSFCell cell4 = row4.createCell(5);
+            cell4.setCellFormula("SUM(C5:E5)");
+
+            HSSFRow row5 = sheet.createRow((short) 5);
+            row5.createCell(0).setCellValue(player2);
+            row5.createCell(1).setCellValue(contestant5);
+            HSSFCell cell5 = row5.createCell(5);
+            cell5.setCellFormula("SUM(C6:E6)");
+
+            HSSFRow row6 = sheet.createRow((short) 6);
+            row6.createCell(0).setCellValue(player2);
+            row6.createCell(1).setCellValue(contestant6);
+            HSSFCell cell6 = row6.createCell(5);
+            cell6.setCellFormula("SUM(C7:E7)");
+
+            HSSFRow row7 = sheet.createRow((short) 7);
+            row7.createCell(0).setCellValue(player3);
+            row7.createCell(1).setCellValue(contestant7);
+            HSSFCell cell7 = row7.createCell(5);
+            cell7.setCellFormula("SUM(C8:E8)");
+
+            HSSFRow row8 = sheet.createRow((short) 8);
+            row8.createCell(0).setCellValue(player3);
+            row8.createCell(1).setCellValue(contestant8);
+            HSSFCell cell8 = row8.createCell(5);
+            cell8.setCellFormula("SUM(C9:E9)");
+
+            HSSFRow row9 = sheet.createRow((short) 9);
+            row9.createCell(0).setCellValue(player3);
+            row9.createCell(1).setCellValue(contestant9);
+            HSSFCell cell9 = row9.createCell(5);
+            cell9.setCellFormula("SUM(C10:E10)");
+
+            FileOutputStream fileOut = new FileOutputStream("fantasy.csv");
+            workbook.write(fileOut);
+            fileOut.close();
+            System.out.println("Your csv file has been created");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+    }
+    
+    public static String getBook(String path) {
+        try{
+        FileInputStream file = new FileInputStream(new File(path));
+        HSSFWorkbook workbook = new HSSFWorkbook(file);
+        Sheet sheet = workbook.getSheetAt(0);
+          for (Row row : sheet) {
+                // Itera sobre las celdas de la fila actual
+                for (Cell cell : row) {
+                    // Imprime el contenido de la celda
+                    if(cell.equals(row.getCell(6))){
+                        break;
+                    }else{
+                        switch (cell.getCellType()) {
+                            case STRING:
+                                String cellValue = new String(cell.getStringCellValue());
+                                System.out.print(cellValue + "\t");
+                                break;
+                            case NUMERIC:
+                                System.out.print(Double.parseDouble(cell.getNumericCellValue() + "\t"));
+                                break;
+                            default:
+                                System.out.print(" \t");
+                        }
+                    }
+                }
+                System.out.println(); // Salto de línea después de cada fila
+            }
+            
+            // Cierra el flujo de entrada
+        file.close();
+        workbook.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static String getResult(String path){
+        try{
+        FileInputStream file = new FileInputStream(new File(path));
+        HSSFWorkbook workbook = new HSSFWorkbook(file);
+        Sheet sheet = workbook.getSheetAt(0);
+          for (Row row : sheet) {
+                // Itera sobre las celdas de la fila actual
+                System.out.print(row.getCell(0) + "\t");
+                System.out.print(row.getCell(1) + "\t");
+                System.out.print(row.getCell(6) + "\t");
+                System.out.println(); // Salto de línea después de cada fila
+            }
+            
+            // Cierra el flujo de entrada
+        file.close();
+        workbook.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static String modifyBook(String path, String week){
+        try{
+            Scanner points = new Scanner(System.in);
+            int weekNumber;
+            switch(week){
+                case "week1":
+                    weekNumber = 2;
+                    break;
+                case "week2":
+                    weekNumber = 3;
+                    break;
+                case "week3":
+                    weekNumber = 4;
+                    break;
+                case "result":
+                    weekNumber = 6;
+                    break;
+                default:
+                    return "Sorry, your week is not allowed";
+            }
+            
+            FileInputStream filemodi = new FileInputStream(new File(path));
+            HSSFWorkbook workbook = new HSSFWorkbook(filemodi);
+            Sheet sheet = workbook.getSheetAt(0);
+            
+            HSSFRow row1 = (HSSFRow) sheet.getRow((short) 1);
+            System.out.println("How many points will you add to " + row1.getCell(1));
+            int p1 = points.nextInt();
+            row1.createCell(weekNumber).setCellValue(p1);
+            
+            HSSFRow row2 = (HSSFRow) sheet.getRow((short) 2);
+            System.out.println("How many points will you add to " + row2.getCell(1));
+            int p2 = points.nextInt();
+            row2.createCell(weekNumber).setCellValue(p2);
+            
+            HSSFRow row3 = (HSSFRow) sheet.getRow((short) 3);
+            System.out.println("How many points will you add to " + row3.getCell(1));
+            int p3 = points.nextInt();
+            row3.createCell(weekNumber).setCellValue(p3);
+            
+            HSSFRow row4 = (HSSFRow) sheet.getRow((short) 4);
+            System.out.println("How many points will you add to " + row4.getCell(1));
+            int p4 = points.nextInt();
+            row4.createCell(weekNumber).setCellValue(p4);
+            
+            HSSFRow row5 = (HSSFRow) sheet.getRow((short) 5);
+            System.out.println("How many points will you add to " + row5.getCell(1));
+            int p5 = points.nextInt();
+            row5.createCell(weekNumber).setCellValue(p5);
+            
+            HSSFRow row6 = (HSSFRow) sheet.getRow((short) 6);
+            System.out.println("How many points will you add to " + row6.getCell(1));
+            int p6 = points.nextInt();
+            row6.createCell(weekNumber).setCellValue(p6);
+            
+            HSSFRow row7= (HSSFRow) sheet.getRow((short) 7);
+            System.out.println("How many points will you add to " + row7.getCell(1));
+            int p7 = points.nextInt();
+            row7.createCell(weekNumber).setCellValue(p7);
+            
+            HSSFRow row8 = (HSSFRow) sheet.getRow((short) 8);
+            System.out.println("How many points will you add to " + row8.getCell(1));
+            int p8 = points.nextInt();
+            row8.createCell(weekNumber).setCellValue(p8);
+            
+            HSSFRow row9 = (HSSFRow) sheet.getRow((short) 9);
+            System.out.println("How many points will you add to " + row9.getCell(1));
+            int p9 = points.nextInt();
+            row9.createCell(weekNumber).setCellValue(p9);
+            
+            filemodi.close();
+            
+            FileOutputStream outputStream = new FileOutputStream(path);
+            workbook.write(outputStream);
+            workbook.close();
+            outputStream.close();
+            System.out.println("Points have been added to everyone");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static String getWinner(String path){
+        try{
+        FileInputStream file = new FileInputStream(new File(path));
+        HSSFWorkbook workbook = new HSSFWorkbook(file);
+        Sheet sheet = workbook.getSheetAt(0);
+          for (Row row : sheet) {
+                // Itera sobre las celdas de la fila actual
+                if(row.getCell(5).equals(row.getCell(6))){
+                    System.out.println("Contestant " + row.getCell(1) + " is the winner, therefore Player " + row.getCell(0) + " won.");
+                    break;
+                }
+        }
+            
+        file.close();
+        workbook.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
